f8756d00561ba2ae2bb582dcd0d1db0e993b46a5
Viridianavilla22 <69393977+Viridianavilla22@users.noreply.github.com>
2020-08-15 10:59:36 -0600

    clase java 1

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,32 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte x;
+		short y;
+		int z;
+
+		x = 127;
+		y = 32767;
+		z = 21434566;
+		System.out.println(x);
+		System.out.println(y);
+
+		float base;
+		double altura;
+		base = 30.5F;
+		altura = 40.76;
+		System.out.println(base);
+		
+		char letra1='a';
+		String frase="viridiana";
+		System.out.println(frase);
+		
+	
+		
+				
+	}
+
+}
---
+++ SeleniumAutomation/src/test/java/com/javabasic/Operadores.java
@@ -0,0 +1,28 @@
+package com.javabasic;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 1;
+		int y = 2;
+		int suma = x - y;
+		System.out.println(suma);
+
+		int valor1 = 3;
+		int incrementar = valor1 + 5;
+		System.out.println(incrementar);
+
+		int num1 = 3;
+		int num2 = 3;
+		
+		if (num1 > num2) {
+			System.out.println("Los numeros son iguales");
+		}
+
+		else {
+			System.out.println("Los numeros no son iguales");
+		}
+	}
+
+}
